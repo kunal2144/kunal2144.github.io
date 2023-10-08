d61ded6fbeb9f90d23ae5c2be8079d92fd8fbc87
kunal2144 <jkunal2144@gmail.com>
2023-10-08 17:00:00 +0530

    Updated entire website

--- src/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-import Kunal from '../assets/Me_GDSC_Cropped.png'
-
-export default function Header() {
-    return (
-        <div className="img-section">
-            <img id="kunal-img" src={Kunal} alt="An image of Kunal" />
-        </div>
-    )
-}
